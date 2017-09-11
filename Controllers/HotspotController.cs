1ea0254097052403282c04b892151ee429854335
mitchellblom <mitchellblom@gmail.com>
2017-09-11 12:31:42 -0500

    rating controller

---
+++ Controllers/HotspotController.cs
@@ -0,0 +1,139 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using YFinderAPI.Data;
+using YFinderAPI.Models;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Cors;
+
+namespace YFinder.Controllers
+{
+    [Route("api/[controller]")]
+    [EnableCors("AllowWhiteListOrigins")]
+    public class HotspotController : Controller
+    {
+
+        private YFinderContext _context;
+        public HotspotController(YFinderContext ctx)
+        {
+            _context = ctx;
+        }
+
+        // GET all Hotspots from hotspot table
+        [HttpGet]
+        public IActionResult Get(string active)
+        {
+            IQueryable<object> hotspots = from hotspot in _context.Hotspot select hotspot;
+
+            if (hotspots == null)
+            {
+                return NotFound();
+            } 
+
+                return Ok(hotspots);
+        }
+
+        //GET one hotspot from hotspot table
+        [HttpGet("{id}", Name = "GetHotspot")]
+        public IActionResult Get([FromRoute] int id)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            try
+            {
+                Hotspot hotspot = _context.Hotspot.Single(m => m.HotspotId == id);
+
+                if (hotspot == null)
+                {
+                    return NotFound();
+                }
+                
+                return Ok(hotspot);
+            }
+            catch (System.InvalidOperationException ex)
+            {
+                return NotFound();
+            }
+        }
+
+        // POST hotspot values to the hotspot table
+        [HttpPost]
+        public IActionResult Post([FromBody] Hotspot hotspot)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            _context.Hotspot.Add(hotspot);
+            
+            try
+            {
+                _context.SaveChanges();
+            }
+            catch (DbUpdateException)
+            {
+                if (HotspotExists(hotspot.HotspotId))
+                {
+                    return new StatusCodeResult(StatusCodes.Status409Conflict);
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return CreatedAtRoute("GetHotspot", new { id = hotspot.HotspotId }, hotspot);
+        }
+
+    // Checks if the Hotspot has been created or not
+    private bool HotspotExists(int hotspotId)
+    {
+      throw new NotImplementedException();
+    }
+
+    // PUT edited values on existing hotspot
+    [HttpPut("{id}")]
+         public IActionResult Put(int id, [FromBody] Hotspot hotspot)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != hotspot.HotspotId)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(hotspot).State = EntityState.Modified;
+
+            try
+            {
+                _context.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!HotspotExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return new StatusCodeResult(StatusCodes.Status204NoContent);
+        }
+
+    // Delete feature may be in version 2. This may end up as an "active" vs "inactive" hotspot for search results.
+
+    }
+}
---
+++ Controllers/RatingController.cs
@@ -0,0 +1,158 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using YFinderAPI.Data;
+using YFinderAPI.Models;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Cors;
+
+namespace YFinder.Controllers
+{
+    [Route("api/[controller]")]
+    [EnableCors("AllowWhiteListOrigins")]
+    public class RatingController : Controller
+    {
+
+        private YFinderContext _context;
+        public RatingController(YFinderContext ctx)
+        {
+            _context = ctx;
+        }
+
+        // GET all Ratings from rating table
+        [HttpGet]
+        public IActionResult Get(string active)
+        {
+            IQueryable<object> ratings = from rating in _context.Rating select rating;
+
+            if (ratings == null)
+            {
+                return NotFound();
+            } 
+
+                return Ok(ratings);
+        }
+
+        //GET one rating from rating table
+        [HttpGet("{id}", Name = "GetRating")]
+        public IActionResult Get([FromRoute] int id)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            try
+            {
+                Rating rating = _context.Rating.Single(m => m.RatingId == id);
+
+                if (rating == null)
+                {
+                    return NotFound();
+                }
+                
+                return Ok(rating);
+            }
+            catch (System.InvalidOperationException ex)
+            {
+                return NotFound();
+            }
+        }
+
+        // POST rating values to the rating table
+        [HttpPost]
+        public IActionResult Post([FromBody] Rating rating)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            _context.Rating.Add(rating);
+            
+            try
+            {
+                _context.SaveChanges();
+            }
+            catch (DbUpdateException)
+            {
+                if (RatingExists(rating.RatingId))
+                {
+                    return new StatusCodeResult(StatusCodes.Status409Conflict);
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return CreatedAtRoute("GetRating", new { id = rating.RatingId }, rating);
+        }
+
+    // Checks if the Rating has been created or not
+    private bool RatingExists(int ratingId)
+    {
+      throw new NotImplementedException();
+    }
+
+    // PUT edited values on existing rating
+    [HttpPut("{id}")]
+         public IActionResult Put(int id, [FromBody] Rating rating)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != rating.RatingId)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(rating).State = EntityState.Modified;
+
+            try
+            {
+                _context.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!RatingExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return new StatusCodeResult(StatusCodes.Status204NoContent);
+        }
+
+        // Delete ratings... needs limitation to delete only one's own.
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            Rating rating = _context.Rating.Single(r => r.RatingId == id);
+            if (rating == null)
+            {
+                return NotFound();
+            }
+
+            _context.Rating.Remove(rating);
+            _context.SaveChanges();
+
+            return Ok(rating);
+        }
+
+    }
+}
