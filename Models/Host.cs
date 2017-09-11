0ce221e35a8258b099640e0a50eafbc2e6d14ddf
mitchellblom <mitchellblom@gmail.com>
2017-09-11 11:27:39 -0500

    host and hotspot models

---
+++ Models/Host.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace YFinder.Models
+{
+  public class Host
+  {
+    [Key]
+    public int HostId { get; set; }
+
+    [Required]
+    [StringLength(100)]
+    public string Address { get; set; }
+
+    [Required]
+    [StringLength(50)]
+    public string Title { get; set; }
+
+    [Required]
+    public int UserId { get; set; }
+
+    [Required]
+    public User User { get; set; }  // Host user must be Host == 1 (true)
+
+  }
+}
---
+++ Models/Hotspot.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace YFinder.Models
+{
+  public class Hotspot
+  {
+    [Key]
+    public int HotspotId { get; set; }
+
+    [Required]
+    [StringLength(100)]
+    public string Title { get; set; }
+    
+  }
+}
