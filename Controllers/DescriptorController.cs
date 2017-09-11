8d4320fa43f39931378e9d0cbc51b5de5261ce28
mitchellblom <mitchellblom@gmail.com>
2017-09-11 12:20:14 -0500

    descriptor and host controllers

---
+++ Controllers/DescriptorController.cs
@@ -0,0 +1,139 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using YFinderAPI.Data;
+using YFinderAPI.Models;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Cors;
+
+namespace YFinder.Controllers
+{
+    [Route("api/[controller]")]
+    [EnableCors("AllowWhiteListOrigins")]
+    public class DescriptorController : Controller
+    {
+
+        private YFinderContext _context;
+        public DescriptorController(YFinderContext ctx)
+        {
+            _context = ctx;
+        }
+
+        // GET all Descriptors from descriptor table
+        [HttpGet]
+        public IActionResult Get(string active)
+        {
+            IQueryable<object> descriptors = from descriptor in _context.Descriptor select descriptor;
+
+            if (descriptors == null)
+            {
+                return NotFound();
+            } 
+
+                return Ok(descriptors);
+        }
+
+        //GET one descriptor from descriptor table
+        [HttpGet("{id}", Name = "GetDescriptor")]
+        public IActionResult Get([FromRoute] int id)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            try
+            {
+                Descriptor descriptor = _context.Descriptor.Single(m => m.DescriptorId == id);
+
+                if (descriptor == null)
+                {
+                    return NotFound();
+                }
+                
+                return Ok(descriptor);
+            }
+            catch (System.InvalidOperationException ex)
+            {
+                return NotFound();
+            }
+        }
+
+        // POST descriptor values to the descriptor table
+        [HttpPost]
+        public IActionResult Post([FromBody] Descriptor descriptor)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            _context.Descriptor.Add(descriptor);
+            
+            try
+            {
+                _context.SaveChanges();
+            }
+            catch (DbUpdateException)
+            {
+                if (DescriptorExists(descriptor.DescriptorId))
+                {
+                    return new StatusCodeResult(StatusCodes.Status409Conflict);
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return CreatedAtRoute("GetDescriptor", new { id = descriptor.DescriptorId }, descriptor);
+        }
+
+    // Checks if the Descriptor has been created or not
+    private bool DescriptorExists(int descriptorId)
+    {
+      throw new NotImplementedException();
+    }
+
+    // PUT edited values on existing descriptor
+    [HttpPut("{id}")]
+         public IActionResult Put(int id, [FromBody] Descriptor descriptor)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != descriptor.DescriptorId)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(descriptor).State = EntityState.Modified;
+
+            try
+            {
+                _context.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!DescriptorExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return new StatusCodeResult(StatusCodes.Status204NoContent);
+        }
+
+    // No Delete necessary for Descriptors; users will not be deleting them.
+
+    }
+}
---
+++ Controllers/HostController.cs
@@ -0,0 +1,139 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using YFinderAPI.Data;
+using YFinderAPI.Models;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Cors;
+
+namespace YFinder.Controllers
+{
+    [Route("api/[controller]")]
+    [EnableCors("AllowWhiteListOrigins")]
+    public class HostController : Controller
+    {
+
+        private YFinderContext _context;
+        public HostController(YFinderContext ctx)
+        {
+            _context = ctx;
+        }
+
+        // GET all Hosts from host table
+        [HttpGet]
+        public IActionResult Get(string active)
+        {
+            IQueryable<object> hosts = from host in _context.Host select host;
+
+            if (hosts == null)
+            {
+                return NotFound();
+            } 
+
+                return Ok(hosts);
+        }
+
+        //GET one host from host table
+        [HttpGet("{id}", Name = "GetHost")]
+        public IActionResult Get([FromRoute] int id)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            try
+            {
+                Host host = _context.Host.Single(m => m.HostId == id);
+
+                if (host == null)
+                {
+                    return NotFound();
+                }
+                
+                return Ok(host);
+            }
+            catch (System.InvalidOperationException ex)
+            {
+                return NotFound();
+            }
+        }
+
+        // POST host values to the host table
+        [HttpPost]
+        public IActionResult Post([FromBody] Host host)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            _context.Host.Add(host);
+            
+            try
+            {
+                _context.SaveChanges();
+            }
+            catch (DbUpdateException)
+            {
+                if (HostExists(host.HostId))
+                {
+                    return new StatusCodeResult(StatusCodes.Status409Conflict);
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return CreatedAtRoute("GetHost", new { id = host.HostId }, host);
+        }
+
+    // Checks if the Host has been created or not
+    private bool HostExists(int hostId)
+    {
+      throw new NotImplementedException();
+    }
+
+    // PUT edited values on existing host
+    [HttpPut("{id}")]
+         public IActionResult Put(int id, [FromBody] Host host)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != host.HostId)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(host).State = EntityState.Modified;
+
+            try
+            {
+                _context.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!HostExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return new StatusCodeResult(StatusCodes.Status204NoContent);
+        }
+
+    // Delete feature may be in version 2. This may end up as an "active" vs "inactive" host for search results.
+
+    }
+}
